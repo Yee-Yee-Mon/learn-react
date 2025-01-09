295fdc6403b043ac7254082247a68487677d6f1c
Yee-Yee-Mon <yeeyeemon.dev19@gmail.com>
2025-01-09 13:15:30 +0630

    show list using react state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -8,6 +7,21 @@ function App() {
   let [name, setName]= useState("Yee Yee Mon") // return array [getter, setterFun]
   //let [name, setName] array destructuring format
 
+  let[posts, setPosts] = useState([
+    {
+      id : 1,
+      title : "first Post"
+    },
+    {
+      id : 2,
+      title : "Second Post"
+    },
+    {
+      id : 3,
+      title : "Third Post"
+    },
+
+  ])
   let changeName = () =>{
 
     setName("Aung Aung")
@@ -17,6 +31,13 @@ function App() {
     <div className="app">
       <h1>Hello {name}</h1>
       <button onClick={changeName}>change name</button>
+
+      <h1>Posts</h1>
+      <ul>
+        {posts.map((post)=>(
+           <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
    
   );
